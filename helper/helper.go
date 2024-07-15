5fb43b02edd18d4f782683ff63517b5999afeb6c
Ton Arruda <georgem.juniore@gmail.com>
2024-07-15 19:00:05 -0300

    Ajustes

--- helper/helper.go
+++ helper/helper.go
@@ -8,13 +8,13 @@ import (
 
 func GenerateNextCode(lastCode string) string {
 	if lastCode == "" {
-		return "0001" // Primeiro código padrão se nenhum existir
+		return "0001"
 	}
 
 	lastNum, err := strconv.Atoi(lastCode)
 	if err != nil {
 		log.Printf("Error converting last code to integer: %v", err)
-		return "" // Ou outro tratamento de erro conforme necessário
+		return ""
 	}
 
 	nextNum := lastNum + 1
