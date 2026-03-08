991c7927ba4ae4427793a50d8d9f33313569a5e1
Ton Arruda <georgem.juniore@gmail.com>
2026-03-07 23:42:24 -0300

    Ajustes

--- tests/get_Usuario_test.go
+++ tests/get_Usuario_test.go
@@ -37,13 +37,11 @@ func TestGetUsuarios(t *testing.T) {
 			assert.NoError(t, err, "Erro ao deserializar resposta para %s", tc.description)
 
 			for _, usuario := range usuarios {
-				// Validar que o campo "id" está presente, é uma string, segue o formato de UUID e não está vazio
 				assert.Contains(t, usuario, "id", "Campo 'id' ausente na resposta para %s", tc.description)
 				assert.IsType(t, "", usuario["id"], "Campo 'id' deve ser uma string para %s", tc.description)
 				assert.Regexp(t, regexp.MustCompile("^[a-f0-9-]{36}$"), usuario["id"], "ID inválido para %s", tc.description)
 				assert.NotEmpty(t, usuario["id"], "Campo 'id' não deve ser vazio para %s", tc.description)
 
-				// Validar que o campo "codigo" está presente, é uma string e não está vazio
 				assert.Contains(t, usuario, "codigo", "Campo 'codigo' ausente na resposta para %s", tc.description)
 				assert.IsType(t, "", usuario["codigo"], "Campo 'codigo' deve ser uma string para %s", tc.description)
 				assert.NotEmpty(t, usuario["codigo"], "Campo 'codigo' não deve ser vazio para %s", tc.description)
