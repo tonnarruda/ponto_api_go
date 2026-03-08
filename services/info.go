4ff4910d05570b40c9b63ad70d4cb3a807834d35
Ton Arruda <georgem.juniore@gmail.com>
2026-03-08 00:04:27 -0300

    Comot ajustes

--- services/info.go
+++ services/info.go
@@ -13,7 +13,6 @@ func NewInfoService(infoRepo *repositories.InfoRepository) *InfoService {
 	return &InfoService{infoRepository: infoRepo}
 }
 
-// Teste Aqui fiz nada novo
 func (s *InfoService) GetAllInfo() ([]structs.Info, error) {
 	return s.infoRepository.GetAll()
 }
