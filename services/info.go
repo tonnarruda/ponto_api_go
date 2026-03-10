a6c664e5f47c17eb63d4111d53892e70e2ce1eb9
Ton Arruda <georgem.juniore@gmail.com>
2026-03-10 11:58:38 -0300

    Ajustes

--- services/info.go
+++ services/info.go
@@ -2,5 +2,5 @@ package services
 
 // PodeTirarCNH verifica se a pessoa pode tirar a CNH.	Retorna true se tiver 18 anos ou mais.
 func PodeTirarCNH(idade int) bool {
-	return idade > 18
+	return idade >= 18
 }
